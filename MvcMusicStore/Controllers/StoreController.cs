eee4baf5f7b9405375823d16fa94012f4352b6f1
unknown <edeng@GX790-025.paraport.com>
2014-09-22 10:38:20 -0700

    added controllers, updated content

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        //
+        // GET: /Store/Browse
+        public string Browse()
+        {
+            return "Hello from Store.Browse()";
+        }
+
+        //
+        // GET: /Store/Details/5
+        public string Details(int id)
+        {
+            string message = "Store.Details, ID = " + id;
+
+            return message;
+        }
+    }
+}
