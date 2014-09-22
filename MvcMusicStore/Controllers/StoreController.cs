66dbe36b051b7a0809a85b53eb18e68351d743dc
unknown <edeng@GX790-025.paraport.com>
2014-09-22 10:55:45 -0700

    part 3: views and viewmodels completed

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
 {
@@ -10,25 +11,33 @@ namespace MvcMusicStore.Controllers
     {
         //
         // GET: /Store/
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello from Store.Index()";
+            var genres = new List<Genre>
+            {
+                new Genre { Name = "Disco" }, 
+                new Genre { Name = "Jazz" },
+                new Genre { Name = "Rock" }
+            };
+
+            return View(genres);
         }
 
         //
         // GET: /Store/Browse
-        public string Browse()
+        public ActionResult Browse(string genre)
         {
-            return "Hello from Store.Browse()";
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
         }
 
         //
         // GET: /Store/Details/5
-        public string Details(int id)
+        public ActionResult Details(int id)
         {
-            string message = "Store.Details, ID = " + id;
+            var album = new Album { Title = "Album " + id };
 
-            return message;
+            return View(album);
         }
     }
 }
