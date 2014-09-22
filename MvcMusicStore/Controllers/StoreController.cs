38d496d27114055ce13448d242f6cd43479fd283
unknown <edeng@GX790-025.paraport.com>
2014-09-22 11:15:56 -0700

    finished part 4: models and data access

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -9,17 +9,13 @@ namespace MvcMusicStore.Controllers
 {
     public class StoreController : Controller
     {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
         //
         // GET: /Store/
         public ActionResult Index()
         {
-            var genres = new List<Genre>
-            {
-                new Genre { Name = "Disco" }, 
-                new Genre { Name = "Jazz" },
-                new Genre { Name = "Rock" }
-            };
-
+            var genres = storeDB.Genres.ToList();
             return View(genres);
         }
 
@@ -27,7 +23,7 @@ namespace MvcMusicStore.Controllers
         // GET: /Store/Browse
         public ActionResult Browse(string genre)
         {
-            var genreModel = new Genre { Name = genre };
+            var genreModel = storeDB.Genres.Include("Albums").Single(g => g.Name == genre);
             return View(genreModel);
         }
 
@@ -35,7 +31,7 @@ namespace MvcMusicStore.Controllers
         // GET: /Store/Details/5
         public ActionResult Details(int id)
         {
-            var album = new Album { Title = "Album " + id };
+            var album = storeDB.Albums.Find(id);
 
             return View(album);
         }
